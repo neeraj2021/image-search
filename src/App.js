0f8f523d926d2e71ef6fe4393ad662f70848ec5b
Neeraj Kumar Mahawar <neerajkmahawar2017@gmail.com>
2021-08-01 23:55:30 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Gallery from "./components/Gallery";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Imagedetails from "./components/Imagedetails";
 function App() {
   return (
     <>
       <Switch>
-        <Route exact path="/image-search" component={Gallery} />
+        <Route exact path="/">
+          <Redirect to="/image-search" />
+        </Route>
+        <Route to="/image-search" component={Gallery} />
         <Route exact path="/image-search/:id" component={Imagedetails} />
         <Route>Error</Route>
       </Switch>
