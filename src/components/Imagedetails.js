5ef53aff3e376daf79653681374e40617d638ae7
Neeraj Kumar Mahawar <neerajkmahawar2017@gmail.com>
2021-11-03 14:48:33 +0530

    change npm to yarn

--- src/components/Imagedetails.js
+++ src/components/Imagedetails.js
@@ -2,44 +2,38 @@ import "./imagedetails.css";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { getSingleImage } from "./api/Api";
+import { useParams } from "react-router-dom";
 
-function Imagedetails() {
+function Imagedetails({ match }) {
   const [url, setUrl] = useState();
   const [loading, setLoading] = useState(false);
   const location = useLocation();
-  const FindId = () => {
-    let id = "";
-    const url = location.pathname;
-    let i = url.length - 1;
-    while (url[i] !== "/") {
-      id += url[i];
-      i--;
-    }
-    id = id.split("").reverse().join("");
-    return id;
-  };
+
+  const { id } = useParams();
   useEffect(() => {
     const init = async () => {
       setLoading(true);
-      const id = FindId();
       const [error, response] = await getSingleImage(
         localStorage.getItem("key"),
         id
       );
-      console.log(response);
+      console.log("err = ", error);
       setUrl(response.urls.raw);
       setLoading(false);
     };
     init();
   }, []);
+
   if (loading) {
     return <div className="spinner-border text-primary spinner-position"></div>;
   }
+
   return (
     <>
       <div className="box">
         <img src={url} className="imgSize" alt="" />
       </div>
+      )
     </>
   );
 }
